2a872879ca7cd750864e5a778a9a0878eaf8af26
ema@yoda <emabr90@gmail.com>
2021-06-01 11:48:36 +0200

    Add references, add sample input boxes

--- app.py
+++ app.py
@@ -15,26 +15,31 @@ server = app.server
 
 app.layout = html.Div([
     html.H2("Some plots"),
-    html.Div(["R (Ohm): ",
-              dcc.Input(id='my-input', value='50.0', type='text')]),
+    html.Div(["r (Ohm/m): ",
+              dcc.Input(id='res-in', value='21.0e-6', type='text'),]),
+    html.Div(["l (H/m): ",
+              dcc.Input(id='ind-in', value='0.8591e-6', type='text'),]),
+    html.Div(["c (F/m): ",
+              dcc.Input(id='cap-in', value='13e-12', type='text'),]),
+    html.Div(["g (S/m): ",
+              dcc.Input(id='cond-in', value='13.13e-12', type='text'),]),
     html.Br(),
-    html.Div(id='my-output'),
+    html.Div(id='res-out'),
 
     dcc.Graph(id='graph'),
-
 ])
 
 
 @app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
+    Output(component_id='res-out', component_property='children'),
+    Input(component_id='res-in', component_property='value')
 )
 def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+    return 'r (Ohm/m): {}'.format(input_value)
 
 @app.callback(
     Output('graph', 'figure'),
-    Input('my-input', 'value'))
+    Input('res-in', 'value'))
 def update_figure(R):
     fig = ppl.plot_V_I()
     return fig
--- doc/documentation.md
+++ doc/documentation.md
@@ -1,7 +1,7 @@
 # Documentation
 
 ## Info on parameter values
-Data on typical values of the parameter employed (i.e. impedances and other stuff) come from the notes I wrote during the university courses on power transmission.
+Data on typical values of the parameter employed (i.e. impedances and other stuff) come from an educational paper, see ref [1], table 2.
 
 ## Implementation
 ### About matrix vector multiplication with numpy
@@ -50,3 +50,6 @@ np.dot(M,v) :=
 |3   4| |1|   |13|
 ------- ---   ----
 ```
+
+## References
+[1] R. Benato and S. D. Sessa, "The Ossanna’s Theorem for Educational Purposes: Impact of Distributed Parameter Transmission Lines on Power Systems," in IEEE Access, vol. 9, pp. 10577-10585, 2021, doi: 10.1109/ACCESS.2021.3049395.
