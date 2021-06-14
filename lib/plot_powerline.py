Fix other bugs, change units from m to km

lib/plot_powerline.py
@@ -16,7 +16,7 @@ def plot_V_I(k, Zc, L):
     d = np.linspace(0,L, N)
     tr.hybrid(Zc, k, d)
 
-    A, B, C, D = tr.hybrid(Zc, k, d)
+    A, B, C, D = tr.hybrid(k, Zc, d)
 
     ws = np.array([Vs, Is])
     w_l = [ws]